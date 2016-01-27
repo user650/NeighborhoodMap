cdea40be91918e8155c3f169fc3d195af46213af
SS <scstubbs@pacbell.net>
2016-01-26 23:22:46 -0500

    Added infoWindow. Added logic for cliking on the marker directly as opposeed to cliking the mennu.

--- js/app.js
+++ js/app.js
@@ -7,43 +7,73 @@ var Model = {
     {
       	title: 'Coca Cola Factory',
 		latLng: {lat: 33.771126, lng: -84.396454},
-		address: '606-608 Luckie St NW, Atlanta, GA 30313',
+		address: '606-608 Luckie St NW<br>Atlanta, GA 30313',
 		url: 'https://www.worldofcoca-cola.com/',
+		iconImage: 'img/football.png',
 		visible: false
 	},
 	{
 		title:	'Turner Field - Home of the Braves',
 		latLng: {lat: 33.735067, lng: -84.38999433}, 
-		address: '755 Hank Aaron Dr SE, Atlanta, GA 30315',
+		address: '755 Hank Aaron Dr SE<br>Atlanta, GA 30315',
 		url: 'http://atlanta.braves.mlb.com/atl/ballpark/',
+		iconImage: 'img/baseball.png',
 		visible: true
 	},
 	{
 		title:	'Georgia Aquarium!',
 		latLng: {lat: 33.763627, lng: -84.395121},
-		address: '225 Baker St NW, Atlanta, GA 30313',
+		address: '225 Baker St NW<br>Atlanta, GA 30313',
 		url: 'http://www.georgiaaquarium.org/',
+		iconImage: 'img/fish.png',
 		visible: true
 	},
 	{
 		title:	'Six Flags Over Georgia',
 		latLng: {lat: 33.768570, lng: -84.550910},
-		address: '275 Riverside Pkwy, Austell, GA 30168',
+		address: '275 Riverside Pkwy<br>Austell, GA 30168',
 		url: 'https://www.sixflags.com/overgeorgia',
+		iconImage: 'img/rollercoaster.png',
 		visible: true
 	},
 	{	
 		title:	'Lake Lanier Islands',
 		latLng: {lat: 34.177871, lng: -84.030111},
-		address: '7000 LANIER ISLANDS PARKWAY BUFORD, GA 30518',
+		address: '7000 LANIER ISLANDS PARKWAY<br>BUFORD, GA 30518',
 		url: 'http://www.lanierislands.com/',
+		iconImage: 'img/waterski.png',
 		visible: true
 	},
 	{
 		title: 'The Georgia Dome',
 		latLng: {lat: 33.757694, lng: -84.400625},
-		address: '1 Georgia Dome Dr, Atlanta, GA 30313',
+		address: '1 Georgia Dome Dr.<br>Atlanta, GA 30313',
 		url: 'http://www.gadome.com/',
+		iconImage: 'img/football.png',
+		visible: true
+	},
+	{
+		title: 'Echelon Golf Club',
+		latLng: {lat: 34.197036, lng: -84.286404},
+		address: '201 Traditions Dr.<br>Alpharetta, GA 30004',
+		url: 'http://www.echelongolf.com/',
+		iconImage: 'img/golf.png',
+		visible: true
+	},
+	{
+		title: "Hawk's Ridge Golf Club",
+		latLng: {lat: 34.258384, lng: -84.281266},
+		address: '1100 Hawks Ridge Golf Club<br>Ball Ground, GA 30107',
+		url: 'http://www.hawksridge.com/',
+		iconImage: 'img/golf.png',
+		visible: true
+	},
+	{
+		title: 'In Your Dreams Farm',
+		latLng: {lat: 34.200007, lng: -84.311100},
+		address: '17875 Birmingham Hwy<br>Alpharetta, GA 30004',
+		url: 'https://www.facebook.com/In-Your-Dreams-Farm-325164227564058/',
+		iconImage: 'img/horse.png',
 		visible: true
 	}
   ],
@@ -62,10 +92,9 @@ var ViewModel = function () {
 		console.log ('searchString = ' + self.searchString());
 		markerList().forEach(function(markerItem){
 
-			/* taken from sample http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
-			if(beers[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-        		viewModel.beers.push(beers[x]);
-        	} */
+			/* Sample taken from sample http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+			use the indexOf() to search for the searchString within each of the markerList items.  The visible marker will set
+			how / if the marker will display.  */
 
 			if (self.searchString() == '' || markerItem.title.toLowerCase().indexOf(self.searchString().toLowerCase()) >= 0) {
 				markerItem.setVisible(true);
@@ -85,20 +114,25 @@ var ViewModel = function () {
 	};
 
 	//populate the original markerList with google markers using the data from the Model.miltonMarker
-	console.log("loading the observable array with google markers");
-
     Model.miltonMarker.forEach(function(markerItem){
- 		var marker = new google.maps.Marker({
+	  	var marker = new google.maps.Marker({
 					position: markerItem.latLng,
 					draggable: false,
 					animation: google.maps.Animation.DROP,
+					icon: markerItem.iconImage,
 					title: markerItem.title,
-					visible: markerItem.visible
+					visible: markerItem.visible,
+					address: markerItem.address,
+					url: markerItem.url
         	});
- 		console.log("Marker load: " + markerItem.title);
+
+	   	/*set the functions to call in a click */
+	   	marker.addListener('click', function() {
+			self.toggleBounce(marker);
+  		});
+	   	console.log(marker);
         markerList.push(marker);  // push all of the google marker objects to the markerList arrary
     });
-	console.log("end inital load");
 
 	var initMap = function () {
 			//render the inital map
@@ -115,19 +149,27 @@ var ViewModel = function () {
 	}(); // this extra () is needed to force the initMap function to run on applyBindings
 
 	this.toggleBounce = function (clickedMarker) {
-		console.log('Hi, you clicked on this baby: ' + clickedMarker.title);
-
 		/* if the clicked marker is not animated then stop the animation on 
 		all of the markers and then bounce the one that is clikced */
 		if (clickedMarker.getAnimation() == null) {
 			markerList().forEach(function(markerItem){
 				markerItem.setAnimation(null);
 			});
+
+			//bounce the icon
 			clickedMarker.setAnimation(google.maps.Animation.BOUNCE);
+
+			//set the content of the infowindow
+		 	var contentString = '<h3>' + clickedMarker.title + '</h3>' + clickedMarker.address + '<br>' + '<a href="' + clickedMarker.url + '">' + clickedMarker.url + '</a>';
+		  	var infowindow = new google.maps.InfoWindow({
+		    	content: contentString
+		  	});
+	   		infowindow.open(map, clickedMarker);
   		}
   		else {
+  			/* if the current marker is bouncing then keep the infoWindow displayed, but stop the bouncing of the icon */
   			clickedMarker.setAnimation(google.maps.Animation.NULL);
-  		}
+ 		}
   	}
 
 };
