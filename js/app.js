714558698086c2c116d582b86bc24b3fb2ab76f2
SS <scstubbs@pacbell.net>
2016-02-25 23:46:39 -0500

    I was able to get the icon click events to work again.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
    Scott Stubbs December 2015 
    use Milton, GA / North Atlanta as the center of the map */
 
+
 var $wikiElem = $('#wikipedia-links');
 
 //constructor function for a location object
@@ -13,10 +14,59 @@ var Location = function(data) {
 		url: data.url,
 		icon: data.iconImage,
 		draggable: false,
-		animation: google.maps.Animation.DROP,
+		Animation: google.maps.Animation.DROP,
 		visible: true,
 		highlight: ko.observable(false)
 	});
+	this.infoW = ko.observable('Pending search...');
+};
+
+Location.prototype.getWiki = function () {
+    // load wikipedia data
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + clickedMarker.marker.title + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function( response ) {
+            var articleList = response[1];
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.prepend('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+};
+		 	
+// load the infoWindow
+Location.prototype.infoWindowToggle = function () {
+  	infowindow.close();
+ 	var contentString = '<h3>' + clickedMarker.marker.title + '</h3>' + clickedMarker.marker.address + '<br>' + '<a href="' + clickedMarker.marker.url + '">' + clickedMarker.marker.url + '</a>';
+  	infowindow.setContent(contentString);
+	infowindow.open(map, clickedMarker.marker);
+};
+
+Location.prototype.toggleAnimation = function() {
+	console.log('toggle animation : ' + this.marker.title);
+	if (this.marker.getAnimation() == null) {
+		this.marker.setAnimation(google.maps.Animation.BOUNCE);
+		this.marker.highlight = true;
+	} else {
+		this.marker.setAnimation(null);
+		this.marker.highlight = false;
+	};
+};
+
+Location.prototype.stopAnimation = function() {
+	console.log('stop animation : ' + this.marker.title);
+	this.marker.setAnimation(null);
+	this.marker.highlight = false;
 };
 
 var Model = {
@@ -90,7 +140,7 @@ var Model = {
 			latLng: {lat: 34.200007, lng: -84.311100},
 			address: '17875 Birmingham Hwy<br>Alpharetta, GA 30004',
 			url: 'https://www.facebook.com/In-Your-Dreams-Farm-325164227564058/',
-			iconImage: 'img/horse.png'
+			iconImage: 'img/horse.png',
 		}
 	]
 };
@@ -99,32 +149,68 @@ var viewModel = function () {
 	var self = this; 
 	var map; // the google map object
 	self.placeList = ko.observableArray([]); // list of location objects.
-	self.fred = ko.observableArray([
-		{letter: "q"},
-		{letter: "r"},
-		{letter: "s"},
-		{letter: "t"}
-	]);
+	self.searchString = ko.observable('');
 
 	//for each item in the modle array create new locatoin object in the placeList array.
 	var i = 0;
 	Model.markers.forEach(function(myMarker) {
-		console.log('myMarker.title : ' + myMarker.title);
 		self.placeList.push(new Location(myMarker));
-		console.log('self.placeList()[i++].marker.title : ' + self.placeList()[i++].marker.title);
-		//fred.push(myMarker.title);
-		/*try to throw a random listener in there I think.
-	  	placeList()[i++].marker.addListener('click', function() {
-			self.toggleBounce(marker);
-		});*/
 	});
 
-	this.searchString = ko.observable('');
+	var initMap = function () {
+		//render the inital map
+	  	map = new google.maps.Map(document.getElementById('map-container'), {
+	    		center: Model.miltonCenter,
+	    		zoom: 10
+		});
 
-	//this fucntion will update the visible property of the searchList based on the searchString 
-	this.processFilter = function(bob){
+		//Plop the makers on the map; for each marker in the markerList draw it on the map with .setMap
+		self.placeList().forEach(function(markerItem){
+			markerItem.marker.setMap(map);
+	        
+		 	/*TODO here I needed to place the fucntion directly into this event.
+		 	it would be nice to have the event invoke the .toggleBounce function embedded in marker object
+		 	but I could not get this to work. it is not defined at the time that I am declarnig this event */
+		    google.maps.event.addListener(markerItem.marker, 'click', function() {
+				if (markerItem.marker.getAnimation() == null) {
+					markerItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+					markerItem.marker.highlight = true;
+				} else {
+					markerItem.marker.setAnimation(null);
+					markerItem.marker.highlight = false;
+				};
+			});
+
+		});
+
+	    infowindow = new google.maps.InfoWindow({maxWidth: 300});
+
+	}(); // this extra () is needed to force the initMap function to run on applyBindings
+
+	self.toggleBounce = function (clickedMarker) {
+		
+		console.log('clicked list item : ' + clickedMarker.marker.title);
+		/* if the clicked marker is not animated then stop the animation on 
+		all of the markers and then bounce the one that is clikced */
+		clickedMarker.toggleAnimation();
+		
+		//pan to the clicked marker
+		map.panTo(clickedMarker.marker.position);
+		clickedMarker.marker.setMap(map);
+
+		//stop all of the icons that are not clicked
+		self.placeList().forEach(function(markerItem){
+			if (markerItem !== clickedMarker) {
+				markerItem.stopAnimation;
+			}
+		})
+  	};
+  	
+  	//this fucntion will update the visible property of the searchList based on the searchString 
+   self.processFilter = function(bob){
 		placeList().forEach(function(markerItem){
 
+			console.log('bob fucntion');
 			/* Sample taken from sample http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
 			use the indexOf() to search for the searchString within each of the markerList items.  The visible marker will set
 			how / if the marker will display.  */
@@ -135,96 +221,7 @@ var viewModel = function () {
 				markerItem.marker.setVisible(false);
 			}
 		});
-	
-		/* after all of the flags have been updated then trigger a change 
-		   to the markerList so that it will display */
-		var tempList = ko.observableArray(placeList());
-		placeList([]); //clear the current placeList
-		for (var x=0; x < tempList().length; x++){
-		    placeList.push(tempList()[x]); // build it back up
-		};
+
 	};
-
-	var initMap = function () {
-			//render the inital map
-		  	map = new google.maps.Map(document.getElementById('map-container'), {
-		    		center: Model.miltonCenter,
-		    		zoom: 10
-			});
-
-			/*Plop the makers on the map;
-			for each marker in the markerList draw it on the map with .setMap */
-			self.placeList().forEach(function(markerItem){
-				markerItem.marker.setMap(map);
-			});
-
-		    infowindow = new google.maps.InfoWindow({maxWidth: 300});
-
-	}(); // this extra () is needed to force the initMap function to run on applyBindings
-
-/*	//got this from sentry71 code
-	self.filteredArray = ko.computed(function() {
-    	return ko.utils.arrayFilter(placeList(), function(marker) {
-      		return marker.title;
-    	});
-  	}, self);
-*/
-	this.toggleBounce = function (clickedMarker) {
-		
-		/* if the clicked marker is not animated then stop the animation on 
-		all of the markers and then bounce the one that is clikced */
-		if (clickedMarker.getAnimation() == null) {
-			placeList().forEach(function(markerItem){
-				markerItem.marker.setAnimation(null);
-				markerItem.marker.highlight(false);
-			});
-
-			//pan to the clicked marker
-			map.panTo(clickedMarker.position);
-			
-			//bounce the icon
-			clickedMarker.setAnimation(google.maps.Animation.BOUNCE);
-
-			/* hightlight the clicked marker on the list */
-			clickedMarker.highlight(true);
-
-			//load the wikipedia inforamtion
-			self.getWiki(clickedMarker);
-		 	
-		 	// load the infoWindow
-		  	infowindow.close();
-		 	var contentString = '<h3>' + clickedMarker.title + '</h3>' + clickedMarker.address + '<br>' + '<a href="' + clickedMarker.url + '">' + clickedMarker.url + '</a>';
-		  	infowindow.setContent(contentString);
-	   		infowindow.open(map, clickedMarker);
-  		}
-  		else {
-  			/* if the current marker is bouncing then keep the infoWindow displayed, but stop the bouncing of the icon */
-  			clickedMarker.setAnimation(google.maps.Animation.NULL);
- 		}
-  	};
-
-  	this.getWiki = function (clickedMarker){
-	    // load wikipedia data
-	    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + clickedMarker.title + '&format=json&callback=wikiCallback';
-	    var wikiRequestTimeout = setTimeout(function(){
-	        $wikiElem.text("failed to get wikipedia resources");
-	    }, 8000);
-
-	    $.ajax({
-	        url: wikiUrl,
-	        dataType: "jsonp",
-	        jsonp: "callback",
-	        success: function( response ) {
-	            var articleList = response[1];
-	            for (var i = 0; i < articleList.length; i++) {
-	                articleStr = articleList[i];
-	                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-	                $wikiElem.prepend('<li><a href="' + url + '">' + articleStr + '</a></li>');
-	            };
-	            clearTimeout(wikiRequestTimeout);
-	        }
-	    });
-  	};
-};
-
+}
 ko.applyBindings(new viewModel());
